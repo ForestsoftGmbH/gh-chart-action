6460c8a9747c9eb170d067adabfa809d9e78193f
Sebastian <foerster@forestsoft.de>
2022-11-16 09:59:41 +0100

    fix permissions and debug

--- release.sh
+++ release.sh
@@ -3,6 +3,8 @@ set -xe
 releaseDir=$(mktemp -d)
 mkdir -p ~/.ssh
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+chmod 600 ~/.ssh/known_hosts
+ls -al ~/.ssh/
 git clone git@github.com:ForestsoftGmbH/helm-charts.git $releaseDir
 rsync -a --checksum  charts/ $releaseDir/charts
 cd $releaseDir/charts
