0b1fa20168350b8c6dcd1587406a27339d684fb4
Sebastian <foerster@forestsoft.de>
2022-11-16 09:10:07 +0100

    export commit message

--- release.sh
+++ release.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -xe
-export CI_COMMIT_MESSAGE="${{github.event.head_commit.message}}"
 releaseDir=$(mktemp -d)
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 git clone git@github.com:Forestsoft-de/helm-charts.git $releaseDir
