cc491fc532a8b377ef34b7dd7d53ab223f56ee10
Sebastian <foerster@forestsoft.de>
2022-11-16 08:55:11 +0100

    use last commit message

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ CHART_NAME=$1
 ct list-changed  --config .ct/ct.yaml
 ct lint-and-install --config .ct/ct.yaml 
 
-
-helm package helm/${CHART_NAME} -d ./charts/
+mkdir -p ./charts/
+helm package ${CHART_NAME} -d ./charts/
 
