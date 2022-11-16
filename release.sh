865733d17ff27249c1a6080a51d43e65ba9e5eee
Sebastian <foerster@forestsoft.de>
2022-11-16 09:22:13 +0100

    deactivate .ssh. maybe its already set

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -xe
 releaseDir=$(mktemp -d)
-mkdir -p ~/.ssh
-ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+#mkdir -p ~/.ssh
+#ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 git clone git@github.com:Forestsoft-de/helm-charts.git $releaseDir
 rsync -a --checksum  charts/ $releaseDir/charts
 cd $releaseDir/charts
