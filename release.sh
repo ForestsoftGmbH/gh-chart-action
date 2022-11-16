b7a1211009877864a7ae3c73cdc180841a07b979
Sebastian <foerster@forestsoft.de>
2022-11-16 09:30:08 +0100

    remove removal

--- release.sh
+++ release.sh
@@ -2,7 +2,6 @@
 set -xe
 releaseDir=$(mktemp -d)
 mkdir -p ~/.ssh
-ssh-keygen -R github.com
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 git clone git@github.com:ForestsoftGmbH/helm-charts.git $releaseDir
 rsync -a --checksum  charts/ $releaseDir/charts
