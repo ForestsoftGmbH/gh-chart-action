37611b74559f70537b9273a88fb0a8243704d43d
Sebastian <foerster@forestsoft.de>
2022-11-16 10:06:55 +0100

    debug connection

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ mkdir -p ~/.ssh
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 chmod 600 ~/.ssh/known_hosts
 ls -al ~/.ssh/
+ssh -vvvT git@github.com
 git clone git@github.com:ForestsoftGmbH/helm-charts.git $releaseDir
 rsync -a --checksum  charts/ $releaseDir/charts
 cd $releaseDir/charts
