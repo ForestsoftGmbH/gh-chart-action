95d0e2925c2e40a249fef72b504933468e33e1f5
Sebastian <foerster@forestsoft.de>
2022-11-17 13:11:26 +0100

    fix deployment

--- release.sh
+++ release.sh
@@ -5,4 +5,6 @@ git clone git@github.com:ForestsoftGmbH/helm-charts.git $releaseDir
 rsync -a --checksum  charts/ $releaseDir/charts
 cd $releaseDir/charts
 helm repo index . --url https://forestsoftgmbh.github.io/helm-charts/charts
+git config --global user.email "actions@forestsoft.dev"
+git config --global user.name "Github actions pipeline"
 git add . && git commit -m "${CI_COMMIT_MESSAGE:-"Initial commit"}" && git push -f
