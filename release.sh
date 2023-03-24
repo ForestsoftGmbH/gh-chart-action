5a33db1c42d5e40aa69d16b6737c4b616ad96968
Sebastian <foerster@forestsoft.de>
2023-03-24 19:28:21 +0100

    fix: add host key for every run

--- release.sh
+++ release.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -xe
+
+rm /root.ssh/known_hosts || true
+ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts 
+chmod 600 /root/.ssh/known_hosts
+
 releaseDir=$(mktemp -d)
 git clone git@github.com:ForestsoftGmbH/helm-charts.git $releaseDir
 rsync -a --checksum  charts/ $releaseDir/charts
